b5cf3abf26b728633a362b6c693c17f3caeb01e3
Sukyung <sukseo1994@gmail.com>
2025-08-30 14:58:15 -0400

    2nd version

--- two_sum_01.py
+++ two_sum_01.py
@@ -1,4 +1,5 @@
 class Solution:
+    '''
     def twoSum(self, nums: list, target: int):
         numbers = {}
         for key, n in enumerate(nums):
@@ -6,6 +7,14 @@ class Solution:
                 return [key, numbers[target-n]]
             numbers[n] = key
         return [-1, -1]
-    
+    ''' 
+    # Second Version
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        idx_map = {}
+        for i in range(len(nums)):
+            if target-nums[i] in idx_map:
+                return [i, idx_map[target-nums[i]]]
+            idx_map[nums[i]] = i
+        return [-1, -1]
 sol = Solution()
 print(sol.twoSum([2,7,11,15],9))
